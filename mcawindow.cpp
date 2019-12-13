15b3ceb435d70e9ac49864af7e535d87371c6425
Len Hsu <miniair@hotmail.com>
2019-12-13 11:18:21 +0800

    1. change layout style

--- mcawindow.cpp
+++ mcawindow.cpp
@@ -7,6 +7,17 @@ MCAWindow::MCAWindow(QWidget *parent) :
     ui(new Ui::MCAWindow)
 {
     ui->setupUi(this);
+    ui->plotGrid->addWidget(ui->adaGraph, 0, 0, 1, 1);
+    ui->plotGrid->addWidget(ui->adbGraph, 0, 1, 1, 1);
+    ui->settingGrid->addWidget(ui->channelBox, 0, 0, 1, 1);
+    ui->settingGrid->addWidget(ui->scaleXBox,0, 1, 1, 1);
+    ui->settingGrid->addWidget(ui->scaleYBox, 0, 2, 1, 1);
+    ui->settingGrid->addWidget(ui->plotButton, 0, 3, 1, 1);
+    ui->settingGrid->addWidget(ui->networkBox,1, 0, 1, 1);
+    ui->settingGrid->addWidget(ui->commandLabel,2, 0, 1, 1);
+    ui->settingGrid->addWidget(ui->commandEdit, 2, 1, 1, 1);
+    ui->settingGrid->addWidget(ui->commandButton,2, 2, 1, 1);
+    ui->settingBox->setLayout(ui->settingGrid);
     ionet::Network *udpServer = new ionet::Udpserver;
     dp::DataProcessor *adaProc = new dp::AdProcessor(ui->adaGraph, ADA_HH, ADA_HL);
     dp::DataProcessor *adbProc = new dp::AdProcessor(ui->adbGraph, ADB_HH, ADB_HL);
