42d1cabc0f61fac3ed888fcc270f3eb9cb09f3c9
Len <miniair@hotmail.com>
2021-01-02 21:03:20 +0800

    1. Update project

--- adprocessor.h
+++ adprocessor.h
@@ -25,7 +25,7 @@ namespace dp {
         double sDataTemp[SIZE];
         float fData[65536];
         int index = 0;
-        unsigned short pkt_size = 10;
+        unsigned short pkt_size = 50;
         unsigned short mode;
         float meanVal;
         float varVal;
